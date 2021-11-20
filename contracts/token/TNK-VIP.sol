d9b22bea9f3f81873fb55755a3653288feb17618
BattleTanksCity <94745160+BattleTanksCity@users.noreply.github.com>
2021-11-20 17:13:14 +0300

    Update TNK-VIP.sol

--- contracts/token/TNK-VIP.sol
+++ contracts/token/TNK-VIP.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@BattleTanksCity/contracts/token/ERC20/ERC20.sol";
+import "@BattleTanksCity/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@BattleTanksCity/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@BattleTanksCity/contracts/access/Ownable.sol";
+import "@BattleTanksCity/contracts/security/Pausable.sol";
 
 contract BattleTanksCity is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable {
     constructor() ERC20("BattleTanksCity", "TNK-VIP") {
